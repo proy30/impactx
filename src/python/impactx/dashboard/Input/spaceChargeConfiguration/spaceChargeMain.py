3d28d0a2e38d8980bcbaa0c5f654fca3b1f20cf7
Parthib Roy <proy@lbl.gov>
2024-08-27 21:48:31 -0700

    Connected n_cell values, dynamic size, and space charge toggle to simulation
    And minor format update

--- src/python/impactx/dashboard/Input/spaceChargeConfiguration/spaceChargeMain.py
+++ src/python/impactx/dashboard/Input/spaceChargeConfiguration/spaceChargeMain.py
@@ -4,8 +4,15 @@ from ...trame_setup import setup_server
 
 server, state, ctrl = setup_server()
 
+state.dynamic_size = False
 state.max_level = 3
 state.level_fields = []
+state.n_cell = [0.0, 0.0, 0.0]
+
+
+@state.change("space_charge")
+def on_space_charge_change(space_charge, **kwargs):
+    state.dynamic_size = space_charge
 
 
 @state.change("max_level")
@@ -20,6 +27,16 @@ def on_max_level_change(max_level, **kwargs):
             "error_message": "",
         }
         for i in range(num_levels)
+    ]
+
+
+# Updated function to handle float values
+@state.change("n_cell_x", "n_cell_y", "n_cell_z")
+def on_nCell_value_change(n_cell_x, n_cell_y, n_cell_z, **kwargs):
+    state.n_cell = [
+        int(n_cell_x) if n_cell_x else 0.0,
+        int(n_cell_y) if n_cell_y else 0.0,
+        int(n_cell_z) if n_cell_z else 0.0,
     ]
 
 
@@ -56,18 +73,21 @@ class SpaceChargeConfiguration:
                         vuetify.VTextField(
                             label="nCell_X",
                             v_model=("n_cell_x", ""),
+                            type="number",
                             dense=True,
                         )
                     with vuetify.VCol(cols=4, classes="py-0"):
                         vuetify.VTextField(
                             label="nCell_Y",
                             v_model=("n_cell_y", ""),
+                            type="number",
                             dense=True,
                         )
                     with vuetify.VCol(cols=4, classes="py-0"):
                         vuetify.VTextField(
-                            label="nCell_X",
+                            label="nCell_Z",
                             v_model=("n_cell_z", ""),
+                            type="number",
                             dense=True,
                         )
                 with vuetify.VRow(classes="my-0"):
