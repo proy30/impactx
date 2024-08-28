67a7a78964a17b876e4f8cca6b146e66460f250a
Parthib Roy <proy@lbl.gov>
2024-08-27 21:23:06 -0700

    Added basic UI functionality for space charge configuration

--- src/python/impactx/dashboard/Input/spaceChargeConfiguration/spaceChargeMain.py
+++ src/python/impactx/dashboard/Input/spaceChargeConfiguration/spaceChargeMain.py
@@ -4,9 +4,23 @@ from ...trame_setup import setup_server
 
 server, state, ctrl = setup_server()
 
-# -----------------------------------------------------------------------------
-# User-Interface
-# -----------------------------------------------------------------------------
+state.max_level = 3
+state.level_fields = []
+
+
+@state.change("max_level")
+def on_max_level_change(max_level, **kwargs):
+    num_levels = int(max_level) + 1
+
+    state.level_fields = [
+        {
+            "label": f"prob_relative{i+1}",
+            "value": "",
+            "type": "number",
+            "error_message": "",
+        }
+        for i in range(num_levels)
+    ]
 
 
 class SpaceChargeConfiguration:
@@ -17,30 +31,59 @@ class SpaceChargeConfiguration:
         Creates UI content for space charge configuration
         """
 
-        with vuetify.VCard(style="width: 340px; height: 300px"):
-            vuetify.VCardTitle("Space Charge Configuation")
+        with vuetify.VCard(v_show="space_charge", style="width: 340px;"):
+            vuetify.VCardTitle("Space Charge Configuration")
             vuetify.VDivider()
             with vuetify.VCardText():
-                with vuetify.VRow():
-                    with vuetify.VCol():
-                        vuetify.VCheckbox(
-                            label="Space Charge",
+                with vuetify.VRow(classes="my-0"):
+                    with vuetify.VCol(cols=6, classes="py-0"):
+                        vuetify.VCombobox(
+                            v_model=("particle_shape",),
+                            label="Particle Shape",
+                            items=([1, 2, 3],),
+                            dense=True,
+                        )
+                    with vuetify.VCol(cols=6, classes="py-0"):
+                        vuetify.VCombobox(
+                            label="Poisson Solver",
+                            v_model=("poisson_solver", "Multigrid"),
+                            items=(["Multigrid", "FFT"],),
                             dense=True,
                             hide_details=True,
                         )
-                        vuetify.VCheckbox(
-                            label="Dynamic Size",
-                            dense=True,
-                            hide_details=True,
-                        )
-                with vuetify.VRow():
-                    with vuetify.VCol():
+                with vuetify.VRow(classes="my-0"):
+                    with vuetify.VCol(cols=4, classes="py-0"):
                         vuetify.VTextField(
-                            label="mlmg_relative_tolerance",
-                            v_model=("mlmg_relative_tolerance", 1.0e-7),
+                            label="nCell_X",
+                            v_model=("n_cell_x", ""),
                             dense=True,
                         )
+                    with vuetify.VCol(cols=4, classes="py-0"):
                         vuetify.VTextField(
-                            label="mlmg_absolute_tolerance",
-                            v_model=("mlmg_relative_tolerance", 1.0e-7),
+                            label="nCell_Y",
+                            v_model=("n_cell_y", ""),
+                            dense=True,
+                        )
+                    with vuetify.VCol(cols=4, classes="py-0"):
+                        vuetify.VTextField(
+                            label="nCell_X",
+                            v_model=("n_cell_z", ""),
+                            dense=True,
+                        )
+                with vuetify.VRow(classes="my-0"):
+                    with vuetify.VCol(cols=4, classes="py-0"):
+                        vuetify.VSelect(
+                            v_model=("max_level",),
+                            label="Max Level",
+                            items=([0, 1, 2, 3, 4],),
+                            dense=True,
+                        )
+                with vuetify.VRow(classes="my-0"):
+                    with vuetify.VCol(
+                        v_for=("field in level_fields",), cols="auto", classes="py-0"
+                    ):
+                        vuetify.VTextField(
+                            label=("field.label",),
+                            dense=True,
+                            style="width: 125px;",
                         )
